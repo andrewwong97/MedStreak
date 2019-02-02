ccff15fe754868d1be17a103119ee88dcf931865
Kevin <kpeng7@jhu.edu>
2019-02-02 17:23:36 -0500

    Added some endpoints

--- medstreak/api.py
+++ medstreak/api.py
@@ -4,11 +4,30 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class users(Resource):
-    def get(self):
-        return ['andrew', 'jack']
+class User(Resource):
+    def get(self, id=None):
+        if not id == None:
+            #return user information
+            pass
+        else:
+            #return user id
+            pass
 
-api.add_resource(users, '/users')
+    def post(self, id=None):
+        #login with username/password
+        pass
+
+    def put(self, id=None):
+        #signup with user information
+        pass
+
+    def patch(self, id=None)
+
+
+api.add_resource(User, '/user', '/user/<id>')
+
+class Medication(Resource):
+
 
 if __name__ == '__main__':
     app.run(debug=True)
