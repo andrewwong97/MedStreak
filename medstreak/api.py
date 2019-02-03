c996ef76c2d8a817291da59f0c5ad41ae04b825b
Andrew Wong <wongandrew97@gmail.com>
2019-02-03 01:16:19 -0500

    Write id serializer and basic login func

--- medstreak/api.py
+++ medstreak/api.py
@@ -7,9 +7,9 @@ api = Api(app)
 
 from views import *
 
-api.add_resource(resources.Login, '/login')
-api.add_resource(resources.User, '/user', '/user/<string:user_id>')
-api.add_resource(resources.Medication, '/med', '/med/<string:med_id>')
+api.add_resource(resources.Login, '/api/login')
+api.add_resource(resources.User, '/api/user', '/api/user/<string:user_id>')
+api.add_resource(resources.Medication, '/api/med', '/api/med/<string:med_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- medstreak/views.py
+++ medstreak/views.py
@@ -3,11 +3,3 @@ from database import getDB
 from bson.json_util import dumps
 from api import app
 
-@app.route('/login')
-def login():
-    return 'login'
-
-@app.route('/foo')
-def foo():
-    db = getDB()
-    return dumps(db.users.find())
